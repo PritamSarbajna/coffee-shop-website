4fcb3b38fea685c1981e777562fd47c994bfafb1
Pritam Sarbajna <pritamsarbajna10@gmail.com>
2022-07-13 18:31:10 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let navbar = document.querySelector('.navbar');
+let menuBtn = document.getElementById('menu-btn');
+
+menuBtn.addEventListener('click',()=>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+})
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
